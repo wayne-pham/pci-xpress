3e7854c953e7489a32cc9aee2cdab7e0374deb66
Samip Vaidh <svd246@gmail.com>
2021-04-25 18:59:12 -0400

    added executes for cust, cust acc, manuf

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlite3 import Error
 database = sqlite3.connect("pythonsqlite.db")
 cursor = database.cursor()
 
-### CREATE TABLES ###
+############ TABLES #############
 
 # Store
 cursor.execute("DROP TABLE IF EXISTS STORE")
@@ -58,8 +58,6 @@ CREATE TABLE CUSTOMER_ACCOUNT
   PRIMARY KEY (Account_ID)
 ); """
 cursor.execute(customer_account)
-
-
 
 # Aftermarket Inventory
 cursor.execute("DROP TABLE IF EXISTS AFTERMARKET_INVENTORY")
@@ -143,25 +141,26 @@ cursor.execute(gpu)
 
 
 
-### INSERT DATA ###
+############ INSERT DATA #############
 
 # Store
 cursor.execute("INSERT INTO STORE VALUES(?, ?, ?, ?, ?, ?);", ('PCIXpress','1234 Capacitor Rd','Indianapolis','IN', 12345, 1234567890))
 
 # Stock Inventory
-cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?)", ('RTX 3090 Founders Edition', 10, 'PCIXpress'))
-cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?)", ('RTX 3080 Founders Edition', 9, 'PCIXpress'))
-cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?)", ('RTX 3070 Founders Edition', 8, 'PCIXpress'))
-cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?)", ('RTX 3060 Founders Edition', 7, 'PCIXpress'))
-cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?)", ('RTX 3060 Ti Founders Edition', 6, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('RTX 3090 Founders Edition', 10, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('RTX 3080 Founders Edition', 9, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('RTX 3070 Founders Edition', 8, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('RTX 3060 Founders Edition', 7, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('RTX 3060 Ti Founders Edition', 6, 'PCIXpress'))
+cursor.execute("INSERT INTO STOCK_INVENTORY VALUES(?,?,?);", ('Radeon RX 6700XT', 5, 'PCIXpress'))
 
-# After market inventory
-cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?)", ('RTX 3090 Founders Edition', 10, 'PCIXpress'))
-cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?)", ('RTX 3080 Founders Edition', 9, 'PCIXpress'))
-cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?)", ('RTX 3070 Founders Edition', 8, 'PCIXpress'))
-cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?)", ('RTX 3060 Founders Edition', 7, 'PCIXpress'))
-cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?)", ('RTX 3060 Ti Founders Edition', 6, 'PCIXpress'))
-
+# Aftermarket Inventory
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('RTX 3090 Founders Edition', 10, 'PCIXpress'))
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('RTX 3080 Founders Edition', 9, 'PCIXpress'))
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('RTX 3070 Founders Edition', 8, 'PCIXpress'))
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('RTX 3060 Founders Edition', 7, 'PCIXpress'))
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('RTX 3060 Ti Founders Edition', 6, 'PCIXpress'))
+cursor.execute("INSERT INTO AFTERMARKET_INVENTORY VALUES(?,?,?);", ('Radeon RX 6700XT', 5, 'PCIXpress'))
 
 # GPU
 cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('RTX 3090 Founders Edition', 1500.0, 600.0, 'Ampere', 1000, 24, 1395, 'Nvidia'))
@@ -169,7 +168,29 @@ cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('RTX 3080 Founders E
 cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('RTX 3070 Founders Edition', 500.0, 200.0, 'Ampere', 650, 8, 1500, 'Nvidia'))
 cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('RTX 3060 Founders Edition', 330.0, 132.0, 'Ampere', 550, 12, 1320, 'Nvidia'))
 cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('RTX 3060 Ti Founders Edition', 400.0, 160.0, 'Ampere', 600, 8, 1410, 'Nvidia'))
+cursor.execute("INSERT INTO GPU VALUES(?,?,?,?,?,?,?,?);", ('Radeon RX 6700XT', 479.0, 287.0, 'Big Navi', 600, 12, 2321, 'AMD'))
 
+# Customer
+cursor.execute("INSERT INTO CUSTOMER VALUES(?,?,?,?,?,?);", (1, 'CREDIT', 899.00, 1, 5161889457891231, 'PCIXpress'))
+cursor.execute("INSERT INTO CUSTOMER VALUES(?,?,?,?,?,?);", (2, 'DEBIT', 1800.00, 2, 9875458612258549, 'PCIXpress'))
+cursor.execute("INSERT INTO CUSTOMER VALUES(?,?,?,?,?,?);", (3, 'CREDIT', 3000.00, 3, 5421649879865465, 'PCIXpress'))
+cursor.execute("INSERT INTO CUSTOMER VALUES(?,?,?,?,?,?);", (4, 'CREDIT', 4000.00, 4, 8916854489491213, 'PCIXpress'))
+cursor.execute("INSERT INTO CUSTOMER VALUES(?,?,?,?,?,?);", (5, 'DEBIT', 500.00, 1, 5454897425689864, 'PCIXpress'))
+
+# Customer Account
+cursor.execute("INSERT INTO CUSTOMER_ACCOUNT VALUES(?,?,?,?,?,?,?,?,?);", (1, 'Blub@pcixpress.com', 1234567, '1234 Nulaxy Ave', 'Indianapolis', 'IN', 46589, 'Blub', '12/11/2000')) 
+cursor.execute("INSERT INTO CUSTOMER_ACCOUNT VALUES(?,?,?,?,?,?,?,?,?);", (2, 'Klee@pcixpress.com', 7654321, '6456 Jumpty dumpty Rd', 'Mondstadt', 'MD', 97897, 'Klee', '12/11/2010')) 
+cursor.execute("INSERT INTO CUSTOMER_ACCOUNT VALUES(?,?,?,?,?,?,?,?,?);", (3, 'Nora@pcixpress.com', 6762655, '4564 Imaginary ln', 'Mondstadt', 'MD', 56486, 'Nora', '12/11/2010')) 
+cursor.execute("INSERT INTO CUSTOMER_ACCOUNT VALUES(?,?,?,?,?,?,?,?,?);", (4, 'Fatooey@pcixpress.com', 1666666, '4898 Tsarista Ct', 'Zapolyarny Palace', 'SN', 79786, 'Fatooey', '12/11/1990')) 
+cursor.execute("INSERT INTO CUSTOMER_ACCOUNT VALUES(?,?,?,?,?,?,?,?,?);", (5, 'Lapis@pcixpress.com', 1564897, '6878 Jade ln', 'Minlin', 'LY', 45644, 'Lapis', '12/11/1000'))
+
+# Employee
+cursor.execute("INSERT INTO EMPLOYEE VALUES(?,?,?,?,?,?,?,?,?);", (1, 'Wayne Pham', 200.00, '3432 West Haven Dr.', 'Indianapolis', 'IN', 47899, '10/12/1999', 'PCIXpress'))
+cursor.execute("INSERT INTO EMPLOYEE VALUES(?,?,?,?,?,?,?,?,?);", (2, 'Samip Vaidh', 200.00,'4654 Sugar Weight Dr.', 'Indianapolis', 'IN', 56481, '05/28/1994', 'PCIXpress'))
+
+# Manufacturer
+cursor.execute("INSERT INTO MANUFACTURER VALUES(?,?,?,?,?,?,?);", ('Nvidia', 18007976530, '2788 San Tomas Expressway', 'Santa Clara', 'CA', 95050, 'PCIXpress'))
+cursor.execute("INSERT INTO MANUFACTURER VALUES(?,?,?,?,?,?,?);", ('AMD', 18772841566, '2485 Augustine Road', 'Santa Clara', 'CA', 95054, 'PCIXpress'))
 
 database.commit()
 
